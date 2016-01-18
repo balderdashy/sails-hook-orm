8be09f71774375946700b1af517178622b9001e8
Mike McNeil <mike@balderdash.co>
2016-01-18 11:52:45 -0600

    Missing file.

---
+++ constants/adapter-not-installed.error.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+
+  code: 'E_ADAPTER_NOT_INSTALLED',
+
+
+  inputs: {
+
+    adapterPackageName: {
+      example: 'sails-mysql',
+      required: true
+    },
+
+    datastoreIdentity: {
+      example: 'localmysql'
+    }
+
+  },
+
+
+  template:
+  'There was an error attempting to load `<%=adapterPackageName%>`.\n'+
+  '<% if (typeof datastoreIdentity !== \'undefined\') { %>(Sails attempted to load this adapter because it is referenced by a datastore (`<%=datastoreIdentity%>`) in this app\'s configuration)\n<% } %>'+
+  'It looks like `<%=adapterPackageName%>` is not installed in this app (at least it is not in the expected path within the `node_modules/` directory).\n'+
+  '\n'+
+  'To install, run:\n'+
+  ''
+  // 'Error details:\n'+
+  // '<% originalErrorStackTrace %>'
+  // 'The installed `<%=adapterPackageName%>` adapter appears to be designed for an earlier version of Sails\n'+
+  // '(it has a `registerCollection()` method, meaning it is for Sails version 0.9.x and below).\n'+
+  // 'Since you\'re running a newer version of Sails, the installed version of this adapter probably isn\'t going to work.\n'+
+  // 'Please visit the documentation for this adapter (e.g. on GitHub) to see if a new version has been released\n'+
+  // 'with support for Sails v0.10 and up, and/or do a search for other community adapters for this database.'+
+  // '<% if (typeof datastoreIdentity !== \'undefined\') { %>\n(Sails attempted to load this adapter because it is referenced by a datastore (`<%=datastoreIdentity%>`) in this app\'s configuration)<% } %>'
+
+
+};
