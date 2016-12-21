533dfc66eb49c119165d6954b96e14bd6f1af8ba
Scott Gress <scottmgress@gmail.com>
2016-12-21 15:40:18 -0600

    Check / massage validations in attributes

---
+++ constants/deprecated-validations.list.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  'after': 'isAfter',
+  'before': 'isBefore',
+  'creditcard': 'isCreditCard',
+  'email': 'isEmail',
+  'in': 'isIn',
+  'enum': 'isIn',
+  'ip': 'isIP',
+  'notIn': 'isNotIn',
+  'url': 'isURL',
+  'uuid': 'isUUID',
+  'integer': 'isInteger'
+
+};
